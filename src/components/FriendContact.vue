b2587eb34f79ffb940c27b9d0ccf56f3078ceda7
michael-kestler <michael.s.kestler@gmail.com>
2023-06-09 16:31:37 -0400

    initial push of app project

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+<li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{friend.phone}}</li>
+        <li><strong>Email:</strong>{{friend.email}}</li>
+    </ul>
+</li>
+</template>
+
+<script>
+    export default {
+    data(){
+        return{
+        detailsAreVisible: false,
+        friend: {
+            id: 'manuel',
+            name: 'Manuel Lorenz',
+            phone: '954-749-1358',
+            email: 'manuel@locohost'
+        },
+       
+        }
+        },
+        methods: {
+            toggleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    }
+
+</script>
+
+
