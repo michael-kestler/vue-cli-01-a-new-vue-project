81710de8b9eba0a4124bee3792e143c510123114
michael-kestler <michael.s.kestler@gmail.com>
2023-06-15 14:28:50 -0400

    added methods for events and bound the input fields using v-model

--- src/components/NewFriend.vue
+++ src/components/NewFriend.vue
@@ -1,16 +1,16 @@
 <template>
-    <form>
+    <form @submit.prevent="submitData">
         <div>
             <label>Name</label>
-            <input type="text"/>
+            <input v-model="enteredName" type="text"/>
         </div>
         <div>
             <label>Phone</label>
-            <input type="tel"/> 
+            <input v-model="enteredPhone" type="tel"/> 
         </div>
         <div>
             <label>Email</label>
-            <input type="email"/>
+            <input v-model="enteredEmail" type="email"/>
         </div>
         <div>
             <button>Add Contact</button>
@@ -19,6 +19,21 @@
 </template>
 
 <script>
-export default {};
+export default {
+    emits: ['add-contact'],
+    data(){
+        return{
+            enteredName:'',
+            enteredPhone:'',
+            enteredEmail:''
+        }
+    },
+
+    methods: {
+        submitData() {
+            this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
+        }
+    }
+};
 
 </script>
