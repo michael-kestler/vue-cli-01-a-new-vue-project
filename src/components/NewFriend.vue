9a1f977bc633ada4bc46f4d2eed448aa52a6ef8b
michael-kestler <michael.s.kestler@gmail.com>
2023-06-15 13:42:37 -0400

    created vue component file and form to accept user input

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,20 @@
+<template>
+    <form>
+        <div>
+            <lablel>Name</lablel>
+            <input type="text"/>
+        </div>
+        <div>
+            <label>Phone</label>
+            <input type="tel"/> 
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email"/>
+        </div>       
+    </form>
+</template>
+
+<script>
+
+</script>
